297957f1af14fdae240b81eb2fade59e018a8188
Cristiano Silva <p534722@mail.caixa>
2018-07-20 10:13:35 -0300

    my commit

---
+++ CourseWebDeveloper/Controllers/RestauranteController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseWebDeveloper.Controllers
+{
+    public class RestauranteController : Controller
+    {
+        //
+        // GET: /Restaurante/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ CourseWebDeveloper/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CourseWebDeveloper.MvcApplication" Language="C#" %>
